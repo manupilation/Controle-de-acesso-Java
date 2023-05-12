32c74989f5015fa4f65b85683df7600526770020
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-05-12 19:37:17 -0300

    refactor: emporcalha o código porque não pode modularizar

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,7 +1,9 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.Short;
 
 /**
  * Método principal.
@@ -16,43 +18,61 @@ public class Principal {
     ArrayList<Short> childe = new ArrayList<Short>();
     ArrayList<Short> adult = new ArrayList<Short>();
     ArrayList<Short> senior = new ArrayList<Short>();
+    Scanner scanner = new Scanner(System.in);
 
     do {
-      Scanner scanner = new Scanner(System.in);
-      Messages.entryMessage();
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      System.out.println("1 - Acessar o estabelecimento");
+      System.out.println("2 - Finalizar sistema e mostrar relatório");
 
       short option = scanner.nextShort();
 
       if (option == 1) {
-        Messages.ageRequest();
+        System.out.println("Entre com a sua idade:");
         short age = scanner.nextShort();
-        short ageRating = AgeSystem.ageValidator(age);
         
-        if (ageRating == 0) {
-          AgeSystem.register(childe, age);
-        } else if (ageRating == 1) {
-          AgeSystem.register(adult, age);
+        if (age < 18) {
+          childe.add(age);
+          System.out.println("Pessoa cliente menor de idade, catraca liberada!");
+        } else if (age >= 18 && age < 50) {
+          adult.add(age);
+          System.out.println("Pessoa adulta, catraca liberada!");
         } else {
-          AgeSystem.register(senior, age);
+          senior.add(age);
+          System.out.println("Pessoa adulta a partir de 50, catraca liberada!");
         }
 
       } else if (option == 2) {
         reportMode = true;
-        scanner.close();  
   
       } else {
-        Messages.invalidMessage();
+        System.out.println("Entre com uma opção válida");
       }
 
     } while (!reportMode);
     
-    int total = AgeSystem.calcTotal(childe, adult, senior);
-    Messages.headQuantity();
-    Messages.quantityBody(childe.size(), adult.size(), senior.size());   
+    scanner.close();
+    int total = childe.size() + adult.size() + senior.size();
+    int childeSize = childe.size();
+    int adultSize = adult.size();
+    int seniorSize = senior.size();
     
-    Messages.headPercentual();
-    Messages.percentageBody(childe.size(), adult.size(), senior.size(), total);
+    System.out.println("----- Quantidade -----");
+    System.out.println("menores: " + childeSize);
+    System.out.println("adultas: " + adultSize);
+    System.out.println("a partir de 50: " + seniorSize + "\n ");
     
-    Messages.total(total);  
+    System.out.println("----- Percentual -----");
+    DecimalFormat decimalFormat = new DecimalFormat("00.0");
+
+    String menoresPercentage = decimalFormat.format((double) childeSize * 100 / total);
+    String adultosPercentage = decimalFormat.format((double) adultSize * 100 / total);
+    String seniorsPercentage = decimalFormat.format((double) seniorSize * 100 / total);
+
+    System.out.println("menores: " + menoresPercentage + "%");
+    System.out.println("adultas: " + adultosPercentage + "%");
+    System.out.println("a partir de 50: " + seniorsPercentage + "%" + "\n ");
+    
+    System.out.println("TOTAL: " + total);
   }
 }

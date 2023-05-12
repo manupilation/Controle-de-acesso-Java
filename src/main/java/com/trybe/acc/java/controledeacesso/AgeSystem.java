18367984b11860565a42bf1c96df9d0d1602fb0a
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-05-12 19:01:49 -0300

    feat: AgeSystem class

--- src/main/java/com/trybe/acc/java/controledeacesso/AgeSystem.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/AgeSystem.java
@@ -1,20 +1,53 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.util.ArrayList;
+
 /** 
  * AgeSystem Class to manage users age classification.
  * */
 public class AgeSystem {
 
   /**
+   * Age record method.
+   **/
+  public static ArrayList<Short> register(ArrayList<Short> list, short newAge) {
+    list.add(newAge);
+    
+    return list;
+  }
+
+  /**
    * ageValidator method to determine classification of user.
    */
-  public static void ageValidator(short age) {
+  public static short ageValidator(short age) {
     if (age < 18) {
       Messages.childeMessage();
-    } else if (age > 18 && age < 49) {
+      return 0;
+    } else if (age >= 18 && age < 50) {
       Messages.adultMessage();
+      return 1;
     } else {
       Messages.seniorMessage();
+      return 2;
     }
   }
+  
+  /**
+   * Method that returns total value of three array lists.
+   */
+  public static int calcTotal(ArrayList<Short> childe,
+       ArrayList<Short> adult, ArrayList<Short> senior) {
+    int total = childe.size() + adult.size() + senior.size();
+
+    return total;
+  }
+  
+  /**
+   * Method that returns the percentage of an ArrayList in relation to the total.
+   */
+  public static int calcPercentages(ArrayList<Short> list, int total) {
+    return total;
+  }
+
+
 }

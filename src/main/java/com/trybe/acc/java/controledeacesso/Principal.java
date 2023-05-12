3c5d3401b4de587400f6f1be134be761fd5ef630
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-05-12 16:03:28 -0300

    feat: Principal class messages flow

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,11 +1,40 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.util.Scanner;
+
+/**
+ * Método principal.
+ */
 public class Principal {
 
   /**
-   * Método principal.
+   * Método main to register and generate a report.
    */
   public static void main(String[] args) {
-    // ESCREVA SEU CÓDIGO AQUI
+    boolean reportMode = false;
+
+    do {
+      Scanner scanner = new Scanner(System.in);
+      Messages.entryMessage();
+
+      short option = scanner.nextShort();
+      System.out.println(option);
+
+      if (option == 1) {
+        Messages.ageRequest();
+        short age = scanner.nextShort();
+        AgeSystem.ageValidator(age);
+        
+      } else if (option == 2) {
+        reportMode = true;
+        scanner.close();
+  
+      } else {
+        Messages.invalidMessage();
+ 
+      }
+
+    } while (!reportMode);
+
   }
 }

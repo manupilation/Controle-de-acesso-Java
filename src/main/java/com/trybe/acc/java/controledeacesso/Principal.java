3de5f278147f839a0b1652f386c214e6c07b0cb0
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-05-12 19:02:26 -0300

    feat: Principal class

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,5 +1,6 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,29 +13,46 @@ public class Principal {
    */
   public static void main(String[] args) {
     boolean reportMode = false;
+    ArrayList<Short> childe = new ArrayList<Short>();
+    ArrayList<Short> adult = new ArrayList<Short>();
+    ArrayList<Short> senior = new ArrayList<Short>();
 
     do {
       Scanner scanner = new Scanner(System.in);
       Messages.entryMessage();
 
       short option = scanner.nextShort();
-      System.out.println(option);
 
       if (option == 1) {
         Messages.ageRequest();
         short age = scanner.nextShort();
-        AgeSystem.ageValidator(age);
+        short ageRating = AgeSystem.ageValidator(age);
         
+        if (ageRating == 0) {
+          AgeSystem.register(childe, age);
+        } else if (ageRating == 1) {
+          AgeSystem.register(adult, age);
+        } else {
+          AgeSystem.register(senior, age);
+        }
+
       } else if (option == 2) {
         reportMode = true;
-        scanner.close();
+        scanner.close();  
   
       } else {
         Messages.invalidMessage();
- 
       }
 
     } while (!reportMode);
-
+    
+    int total = AgeSystem.calcTotal(childe, adult, senior);
+    Messages.headQuantity();
+    Messages.quantityBody(childe.size(), adult.size(), senior.size());   
+    
+    Messages.headPercentual();
+    Messages.percentageBody(childe.size(), adult.size(), senior.size(), total);
+    
+    Messages.total(total);  
   }
 }
